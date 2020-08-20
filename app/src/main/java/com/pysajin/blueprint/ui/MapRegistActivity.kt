b37d78a5bb62e0e619dd8bca61529e5f77da754c
PyeonSangJin <vustkd157157@naver.com>
2020-08-20 20:25:44 +0900

    fix activity via mvvm

--- app/src/main/java/com/pysajin/blueprint/ui/MapRegistActivity.kt
+++ app/src/main/java/com/pysajin/blueprint/ui/MapRegistActivity.kt
@@ -1,14 +1,29 @@
 package com.pysajin.blueprint.ui
 
+import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.pysajin.blueprint.BR
 import com.pysajin.blueprint.R
-import com.pysajin.blueprint.databinding.ActivityMainBinding
-import com.pysajin.blueprint.viewmodel.MainViewModel
+import com.pysajin.blueprint.databinding.ActivityMapRegistBinding
+import com.pysajin.blueprint.viewmodel.MapRegistViewModel
 
-class MapRegistActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_map_regist) {
-    private lateinit var viewModel: MainViewModel
+class MapRegistActivity : BaseActivity<ActivityMapRegistBinding>(R.layout.activity_map_regist) {
+    private lateinit var viewModel: MapRegistViewModel
     private lateinit var fragmentManager: FragmentManager
     private lateinit var transaction: FragmentTransaction
 
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MapRegistViewModel::class.java)
+        binding.setVariable(BR.mapRegistVM, viewModel)
+
+        this.viewModel.infoActivity.observe(this, Observer {
+            Log.e("Test","MainActivity의 observer setting부분 $it")
+            changeActivity(it, data = this.viewModel.selectedItem)
+        })
+    }
 }
--- app/src/main/java/com/pysajin/blueprint/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/pysajin/blueprint/viewmodel/MainViewModel.kt
@@ -105,6 +105,7 @@ class MainViewModel(application: Application) : BaseViewModel(application) {
     }
 
     fun onPlusClick(){
+        Log.e("ERRRR", "SASDFASDF")
         goMapRegistPage()
     }
 }
